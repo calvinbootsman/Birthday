bfe8353a7a9f90b50e833f2c00d8193b3faed3bc
calvi <calvi@DESKTOP-78AJCPT>
2022-10-11 08:57:30 +0200

    Add project files.

---
+++ Stalvin/Server/Hubs/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace Stalvin.Server.Hubs
+{
+    /* A user sends their username to this controller.
+     * The  
+     * */
+    public class LoginController : Hub
+    {
+        public async Task AddUser(string user)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user);
+        }
+
+        public async Task IsNameAdded(string user, bool isAdded)
+        {
+            await Clients.All.SendAsync("IsNameAddedMessage", user, isAdded);
+        }
+    }
+}
